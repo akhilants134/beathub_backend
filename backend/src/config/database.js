150041d9bc012d430ccf9d874779c865341db79f
Akhilan Theiventhiran <Akhilan@Akhilans-MacBook-Pro.local>
2026-04-29 09:53:26 +0530

    Fix: remove unsupported mongoose options (useNewUrlParser, useUnifiedTopology, db_name)

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -3,17 +3,16 @@ const mongoose = require("mongoose");
 const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
-	throw new Error("MONGO_URI environment variable not set");
+  throw new Error("MONGO_URI environment variable not set");
 }
 
-mongoose.connect(MONGO_URI, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-})
-	.then(() => console.log("MongoDB connected"))
-	.catch((err) => {
-		console.error("MongoDB connection error:", err);
-		process.exit(1);
-	});
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
 
 module.exports = mongoose;
